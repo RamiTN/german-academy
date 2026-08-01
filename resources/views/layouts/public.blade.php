6ecfcacfa1bd5562b9526cb637a2faa9b5c44b8c
RamiTN <ramiabbassi53@gmail.com>
2026-08-01 12:48:18 +0100

    add meta tag

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="google-site-verification" content="1xBqxMyauuiQXIKMweItmjjLY4nuy9phgyzH76dKSdw" />
 
     {{-- SEO: Title --}}
     <title>{{ $seoTitle ?? 'German Academy — Learn German Online A1 to C2' }}</title>
